259d47ba1d5325363715fe1b68d898120e476e86
MolodyGs <ignacio.molina@alumnos.ucn.cl>
2023-11-14 09:29:14 -0300

    Insertar, eliminar, actualizar

--- app/src/main/java/com/example/evaluaciondsm/AdminSQLiteOpenHelper.java
+++ app/src/main/java/com/example/evaluaciondsm/AdminSQLiteOpenHelper.java
@@ -7,17 +7,29 @@ import android.database.sqlite.SQLiteOpenHelper;
 import androidx.annotation.Nullable;
 
 public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
-    public AdminSQLiteOpenHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+
+    private static final String DATABASE_NAME = "evaluacion2DB";
+    private static final int DATABASE_VERSION = 1;
+    public static final String TABLE_PRODUCTS = "products";
+
+    public AdminSQLiteOpenHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-//        Aqui deben crear la tabla
+        sqLiteDatabase.execSQL("CREATE TABLE PRODUCTS (" +
+                "codigo INTEGER PRIMARY KEY," +
+                "precio REAL NOT NULL," +
+                "descripcion TEXT NOT NULL)");
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
 
+        onCreate(sqLiteDatabase);
     }
+
 }
--- app/src/main/java/com/example/evaluaciondsm/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/evaluaciondsm/ui/home/HomeFragment.java
@@ -7,24 +7,45 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.core.database.sqlite.SQLiteDatabaseKt;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.evaluaciondsm.AdminSQLiteOpenHelper;
 import com.example.evaluaciondsm.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
 
+    private EditText codigo, precio, descripcion;
+    int codigoAux;
+    int precioAux;
+    String descripcionAux;
+    SQLiteDatabase db;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        codigo = (EditText) binding.txtCode;
+        precio = (EditText) binding.txtPrice;
+        descripcion = (EditText) binding.txtDescription;
+
+        codigoAux = Integer.getInteger(codigo.getText().toString());
+        precioAux = Integer.getInteger(precio.getText().toString());
+        descripcionAux = descripcion.getText().toString();
+
+        AdminSQLiteOpenHelper helper = new AdminSQLiteOpenHelper(getContext());
+        db = helper.getWritableDatabase();
 
         binding.btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -66,19 +87,67 @@ public class HomeFragment extends Fragment {
     //Metodo para guardar los productos
     public void save() {
 
+        codigoAux = Integer.getInteger(codigo.getText().toString());
+        precioAux = Integer.getInteger(precio.getText().toString());
+        descripcionAux = descripcion.getText().toString();
+
+        if(codigo.getText().toString().isEmpty() || precio.getText().toString().isEmpty() || descripcion.getText().toString().isEmpty())
+        {
+            Toast.makeText(getContext(), "Todos los campos son obligatorios", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        db.execSQL("INSERT INTO products (codigo, precio, descripcion) VALUES (" +
+                ""+codigoAux+"," +precioAux+"," + descripcionAux + " )");
+        Toast.makeText(getContext(), "Los datos fueron ingresados", Toast.LENGTH_LONG).show();
+
     }
 
     //Metodo para buscar productos
     public void search() {
 
+        codigoAux = Integer.getInteger(codigo.getText().toString());
+        precioAux = Integer.getInteger(precio.getText().toString());
+        descripcionAux = descripcion.getText().toString();
+
+        if(codigo.getText().toString().isEmpty())
+        {
+            Toast.makeText(getContext(), "Debe indicar un codigo", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        db.execSQL("SELECT * FROM products WHERE codigo = " + codigoAux);
+        Toast.makeText(getContext(), "", Toast.LENGTH_SHORT).show();
+
     }
 
     public void delete() {
 
+        codigoAux = Integer.getInteger(codigo.getText().toString());
+        precioAux = Integer.getInteger(precio.getText().toString());
+        descripcionAux = descripcion.getText().toString();
+
+        if(codigo.getText().toString().isEmpty())
+        {
+            Toast.makeText(getContext(), "Debe indicar un codigo", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        db.execSQL("DELETE FROM products WHERE codigo = " + codigoAux);
+        Toast.makeText(getContext(), "Elemento Eliminado Exitosamente", Toast.LENGTH_SHORT).show();
     }
 
     public void edit() {
 
+        if(codigo.getText().toString().isEmpty() || precio.getText().toString().isEmpty() || descripcion.getText().toString().isEmpty())
+        {
+            Toast.makeText(getContext(), "Debe indicar un codigo", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        db.execSQL("UPDATE products SET precio = " + precioAux + "WHERE codigo = " + codigoAux);
+        db.execSQL("UPDATE products SET descripcion = " + descripcion + "WHERE codigo = " + codigoAux);
+        Toast.makeText(getContext(), "Los datos fueron modificados exitosamente", Toast.LENGTH_LONG).show();
     }
 
     public void cleanForm() {
